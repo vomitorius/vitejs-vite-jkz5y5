eaf8c4244bce1e729396f23b1617e0ef1ad6556d
vomitorius <csaba.szabo@me.com>
2022-06-17 09:51:11 +0200

    1 file modified, 1 added, 1 deleted

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
 import './style.css';
-import dfLogo from '/kisspng-indian-elephant-african-elephant-elephantidae-anim-aile-5b31860d678455.590351351529972237424.jpg';
-import { setupCounter } from './counter';
+import dfLogo from '/elephant.jpg';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <img src="${dfLogo}" alt="Vite logo" />
   </div>
 `;
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
